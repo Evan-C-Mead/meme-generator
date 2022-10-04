2a41dd04395835554a4a8c85db4deb76fbb84d3c
Evan-C-Mead <evan.c.mead@gmail.com>
2022-10-03 23:26:51 -0500

    small style changes to card

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ export default function Header() {
         <header className="header">
             <img className="header--image" src="../images/troll-face.png" alt="troll-face"/>
             <h2 className="header--title">Meme-Gen</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
 }
