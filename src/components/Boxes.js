f7d31f56823918ba74f1efeac69a7fa3bf895b6f
Evan-C-Mead <evan.c.mead@gmail.com>
2022-10-06 00:22:16 -0500

    dynamic styling with data

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,11 +1,12 @@
 import React from "react";
 import boxes from "../data/boxes";
+import Box from "./Box";
 
 export default function Boxes() {
     const [squares, setSquares] = React.useState(boxes)
 
     const squareElements = squares.map(square => (
-        <div className="box" key={square.id}></div>
+        <Box key={square.id} on={square.on}/>
     ))
 
     return (
