072e0e4f3f67582f091403a41f4c6827d5c8ea8f
Evan-C-Mead <evan.c.mead@gmail.com>
2022-10-07 17:29:01 -0500

    conditional styling with boxes and dark mode indication

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,10 +3,25 @@ import React from "react";
 export default function Box(props) {
     const styles = {
         backgroundColor: props.on ? "#222222" : "transparent",
-        cursor: "pointer"
+        cursor: "pointer",
+        borderColor: props.on ? "black" : "white",
+    }
+
+    const textStyle = {
+        color: props.on ? "white" : "black",
+    }
+
+    let onOff;
+    if (props.on === true) {
+        onOff = "Dark mode on"
+    } else if (props.on === false) {
+        onOff = "Dark mode off"
     }
 
     return (
-        <div style={styles} className="box" onClick={()=>props.toggle(props.id)}></div>
+        <div style={styles} className="box" onClick={() => props.toggle(props.id)}>
+            <h5 className="box-header" style={textStyle}>Box {props.id}</h5>
+            <p className="box-text" style={textStyle}>{onOff}</p>
+        </div>
     )
 }
