8a165d89baee202acbe89c8d176b439639899c97
Evan-C-Mead <evan.c.mead@gmail.com>
2022-10-16 14:06:04 -0500

    form lesson

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 export default function Form() {
-    const [formNameData, setFormNameData] = React.useState({firstName: "", lastName: "", email: ""})
+    const [formNameData, setFormNameData] = React.useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        info: "",
+        isFriendly: true
+    })
 
     function handleChange(event) {
+        const {name, value, type, checked} = event.target
         setFormNameData(prevFormData => {
             return {
                 ...prevFormData,
-            [event.target.name]: event.target.value
+                [name]: type === "checkbox" ? checked : value
             }
         })
     }
@@ -15,10 +22,13 @@ export default function Form() {
     console.log(formNameData)
 
     return (
-        <form className="form-name">
+        <form className="form-name-info">
             <input type="text" placeholder="first" onChange={handleChange} name="firstName" value={formNameData.firstName}/>
             <input type="text" placeholder="last" onChange={handleChange} name="lastName" value={formNameData.lastName}/>
             <input type="text" placeholder="email" onChange={handleChange} name="email" value={formNameData.email}/>
+            <textarea id="form-info" placeholder="notes" onChange={handleChange} name="info" value={formNameData.info}/>
+            <input type="checkbox" id="isFriend" onChange={handleChange} name="isFriendly" checked={formNameData.isFriendly}/>
+            <label htmlFor="isFriend">Are you a friend?</label>
         </form>
     )
 }
